a2d7e0668de40117e32a48440947695b68570275
BidishaPal <bidisha9330585004@gmail.com>
2025-07-09 20:11:56 +0530

    Create Legend With Header.py

---
+++ Legend With Header.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 30, 15, 20])
+mylabels = ["Lotus", "Rose", "Sunflower", "Marigold"]
+
+plt.pie(y, labels = mylabels)
+plt.legend(title = "Four Fruits:")
+plt.show()
