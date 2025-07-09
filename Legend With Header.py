e91259ca6d89659b3281c5a315746a7db0ca40b7
BidishaPal <bidisha9330585004@gmail.com>
2025-07-09 20:12:28 +0530

    Update Legend With Header.py

--- Legend With Header.py
+++ Legend With Header.py
@@ -5,5 +5,5 @@ y = np.array([35, 30, 15, 20])
 mylabels = ["Lotus", "Rose", "Sunflower", "Marigold"]
 
 plt.pie(y, labels = mylabels)
-plt.legend(title = "Four Fruits:")
+plt.legend(title = "Four Flowers:")
 plt.show()
