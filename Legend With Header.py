47a7b8a068dc92556703e4fd489f71dd066c8293
BidishaPal <bidisha9330585004@gmail.com>
2025-07-09 20:12:45 +0530

    Update Legend With Header.py

--- Legend With Header.py
+++ Legend With Header.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 y = np.array([35, 30, 15, 20])
-mylabels = ["Lotus", "Rose", "Sunflower", "Marigold"]
+mylabels = ["Lotus", "Rose", "Sun Flower", "Marigold"]
 
 plt.pie(y, labels = mylabels)
 plt.legend(title = "Four Flowers:")
